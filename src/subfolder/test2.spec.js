c56717457da479035f9e5b0d92882cd8f51072ec
Fabio Elizandro and Ronaldo Gomes <just-dev+felizandro+rgomes@tyro.com>
2019-08-01 12:22:41 +1000

    removed supertest from unit test

--- src/subfolder/test2.spec.js
+++ src/subfolder/test2.spec.js
@@ -1,26 +1,21 @@
-const supertest = require('supertest');
 const { test2Method } = require('./test2');
 
 describe('my tests', () => {
     it('should pass', () => {
-        supertest(() => { });
         expect(test2Method()).toBe(true);
     });
 
     it('should pass2', () => {
-        supertest(() => { });
         expect(test2Method()).toBe(true);
     });
 })
 
 describe('my second tests', () => {
     it('should pass', () => {
-        supertest(() => { });
         expect(test2Method()).toBe(true);
     });
 
     it('should pass2', () => {
-        supertest(() => { });
         expect(test2Method()).toBe(true);
     });
 });
